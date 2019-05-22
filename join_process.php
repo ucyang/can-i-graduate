e2ca13ae946a3d0d99878afc78c969e3f738a794
j03y14 <46087226+j03y14@users.noreply.github.com>
2019-05-22 23:55:18 +0900

    회원가입 가능
    회원 가입 후 db에 저장까지 가능

---
+++ join_process.php
@@ -0,0 +1,11 @@
+<?php
+
+  $conn = mysqli_connect('127.0.0.1','root','비밀번호','can_i_graduate');
+  $joinSql = "INSERT INTO members(member_srl,user_id,nickname,email,password,major_srl,admission_year,campus,abeek)VALUES(1,'{$_POST[id]}','{$_POST[nickname]}','{$_POST[email]}','{$_POST[password]}',{$_POST[major]},{$_POST[admission_year]},'{$_POST[campus]}','{$_POST[abeek]}')";
+  if(mysqli_query($conn,$joinSql)===false){
+    echo '회원가입 중 오류가 발생<br>오류 내용 :';
+    echo mysqli_error($conn);
+  }
+  header('Location: index.php');
+
+ ?>
