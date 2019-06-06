c167c0de04d6d2532605b56a204cea648c5a4667
j03y14 <j03y14@gmail.com>
2019-06-06 21:53:50 +0900

    data 클래스 수정
    data 클래스 수정

--- classes/data/Data.class.php
+++ classes/data/Data.class.php
@@ -5,33 +5,5 @@
 class Data
 {
     public static $data = NULL;
-    protected static $file;
 
-    public static function connectFile($uploadFile)
-    {
-        self::$file = fopen("test.csv", "r"));
-
-    }
-
-    public static function parseFile()
-    {
-      $row =1
-      while (($data = fgetcsv(self::$file, 1000, ",")) !== FALSE)
-      {
-
-        if($row>2)
-        {
-
-          $sql = "INSERT INTO attended_lectures(member_srl,lecture_srl, grade) VALUES ({$_SESSION['member_srl']}, $data[3], $data[5])";
-          if(DB::getConn()->query($sql) == false)
-          {
-            echo "error: " . DB::getConn()->error;
-          }
-        }
-
-
-        $row++;
-      }
-
-  }
 }
