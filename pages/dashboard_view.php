a09502cb8c373df378ecfbbcde2e8cae31ff719f
j03y14 <j03y14@gmail.com>
2019-06-08 14:57:43 +0900

    logout O 소문자 처리
    logout O 소문자 처리

--- pages/dashboard_view.php
+++ pages/dashboard_view.php
@@ -139,20 +139,13 @@
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="/?act=logOut">로그아웃</a>
+        <a class="nav-link" href="/?act=logout">로그아웃</a>
       </li>
     </ul>
     <!--~님 안녕하세요-->
     <div class="greeting">                             <!--php로 구현해야 할 것들: 사용자의 이름(name)-->
       <?php
-      $sql = "SELECT nickname FROM members where member_srl = {$_SESSION['user_srl']}";
-    	$sqlResult = DB::getConn()->query($sql);
-    	if($sqlResult==false){
-    		echo "ERROR : ". DB::getConn()->error;
-    	}else{
-    		$memberinfo = $sqlResult->fetch_assoc();
-        
-    	}
+      
 
       echo $memberinfo['nickname'];
 
