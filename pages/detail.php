76461cf7596ccfd45c278a392ce13de51c8b9381
j03y14 <j03y14@gmail.com>
2019-06-08 19:30:30 +0900

    오류 수정
    오류 수정(detailPage를 detail로 변경 하면서 이것은 변경을 안 했음)

--- pages/detail.php
+++ pages/detail.php
@@ -1,7 +1,15 @@
 <?php
 
-	if(! array_key_exists('user_id', $_SESSION)) Header("Location:/?act=login");
+	if(! array_key_exists('user_srl', $_SESSION)) Header("Location:/?act=login");
 
+	$sql = "SELECT nickname FROM members where member_srl = {$_SESSION['user_srl']}";
+	$sqlResult = DB::getConn()->query($sql);
+	if($sqlResult==false){
+		echo "ERROR : ". DB::getConn()->error;
+	}else{
+		$memberinfo = $sqlResult->fetch_assoc();
+
+	}
 	// portfolio, internship, 시험일정 등 관리하기
-	require 'detailPage_view.php';
+	require 'detail_view.php';
 ?>
