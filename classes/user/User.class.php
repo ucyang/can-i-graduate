0c4526a093d0daa0d45c8fe61d56a850ce127230
j03y14 <j03y14@gmail.com>
2019-06-09 22:56:36 +0900

    주석 추가
    주석 추가

--- classes/user/User.class.php
+++ classes/user/User.class.php
@@ -12,8 +12,18 @@ class User
     public static $admission_year;
     public static $campus;
 
+    public static $gpa; //학점
+    public static $gpa_major;//전공 학점
+
+    public static $graduationStatus; //졸업요건 연관배열(english, korean_history,chinese_char,paper,counseling,portfolio,coding_boot_camp,topcit,open_source)
+
+
+
     public static $attended_lectures;
 
+    /*
+    전공, 입학년도, 캠퍼스 정보를 가져와서 저장
+    */
     public static function init(){
       $sql ="SELECT major_srl, admission_year, campus FROM members WHERE member_srl = {$_SESSION['user_srl']}";
       $data= DB::getConn()->query($sql);
@@ -32,11 +42,16 @@ class User
       self::$campus = $userInfo['campus'];
     }
 
+    //public static function get
+
+    /*
+    attended_lectures에서 수강한 과목들을 가져온다.
+    */
     public static function getattendedLectures()
     {
         $sql ="SELECT attended_lectures.lecture_srl, attended_lectures.grade, lectures.course_no, lectures.title, lectures.college, lectures.dept, lectures.course_type, lectures.abeek_type
         FROM attended_lectures INNER JOIN lectures USING(lecture_srl) WHERE member_srl= {$_SESSION['user_srl']}";
-        
+
         $data= DB::getConn()->query($sql);
         if($data)
         {
